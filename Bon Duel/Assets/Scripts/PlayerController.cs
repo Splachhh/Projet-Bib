b3c4bc78620d7ebcf48185d2516ba49a09ada764
Splachhh <thibaut.grandclement@gmail.com>
2014-02-05 08:04:45 +0100

    Intégration de la carotte comme joueur + Ajout du script de déplacement du joueur + ajout du script de déplacement de la caméra

---
+++ Bon Duel/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	CharacterController control;
+
+	private Vector3 moveDirection = Vector3.zero;
+	private Vector3 rotation = Vector3.zero;
+	private float gravity = 1;
+	public float speed = 0.5f;
+	public float rotationFactor = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		control = GetComponent<CharacterController>() as CharacterController;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		moveDirection.y -= gravity * Time.deltaTime;
+		control.Move(moveDirection * Time.deltaTime);
+
+		// Rotation
+		rotation = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+		rotation *= rotationFactor * Time.timeScale;
+		transform.Rotate(rotation);
+	}
+}
